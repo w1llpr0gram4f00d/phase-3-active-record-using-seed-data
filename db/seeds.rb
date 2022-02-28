0bc548aecf8dbcf29b5f16def74e2b243bbe17b3
Marc Cavazos <marcavazos19@icloud.com>
2022-02-28 15:49:56 -0600

    commit tree 1b98ac4e4379806bb7ed48486898ab63d388a440

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 # Add a console message so we can see output when the seed file runs
+50.times do
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
